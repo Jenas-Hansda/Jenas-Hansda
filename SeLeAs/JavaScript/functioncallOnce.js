20974cd17149224a7e14bf394c945636d5a02685
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-02-21 11:27:36 +0530

    Compt 9

---
+++ SeLeAs/JavaScript/functioncallOnce.js
@@ -0,0 +1,11 @@
+var call = function(fn) {
+    let called = false;
+        return function(...args) {
+      if (called) {
+        return undefined;
+      }
+      called = true;
+        return  fn(...args);
+    };
+  }
+  
