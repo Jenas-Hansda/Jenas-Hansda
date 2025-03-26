0372eb1ee0349b1e3987ffa26c0778bdd4bfc458
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-03-26 08:48:25 +0530

    LC107

---
+++ SeLeAs/C++/increasingTriplet.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        bool increasingTriplet(vector<int>& nums) {
+            int n = nums.size();
+            int k = 3;
+            vector<long long> increasing(k, LONG_MAX);
+    
+            // Increasing Subsequence
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < k; ++j) {
+                    if (increasing[j] >= nums[i]) {
+                        increasing[j] = nums[i];
+                        break;
+                    }
+                }
+                if (increasing[k - 1] != LONG_MAX) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+    
