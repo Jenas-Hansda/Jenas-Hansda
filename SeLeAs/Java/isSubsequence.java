f1710c7434a4f401c032d60492eebe7d80367142
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-03-28 06:47:37 +0530

    LCJ109

---
+++ SeLeAs/Java/isSubsequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int ptr_s = 0, ptr_t = 0;
+        while (ptr_s < s.length() && ptr_t < t.length()) {
+            if (s.charAt(ptr_s) == t.charAt(ptr_t)) {
+                ptr_s++;
+            }
+            ptr_t++;
+        }
+        return ptr_s >= s.length();
+    }
+}
