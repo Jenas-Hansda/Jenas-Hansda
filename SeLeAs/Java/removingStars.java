a84b126e092b22c08306447e0769a4df8653c9bf
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-04-10 08:16:04 +0530

    LC123

---
+++ SeLeAs/Java/removingStars.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        // Build the result from the stack
+        StringBuilder result = new StringBuilder();
+        for (char ch : stack) {
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+}
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Solution sol = new Solution();
+//         String input = "leet**cod*e";
+//         String result = sol.removeStars(input);
+//         System.out.println(result);  // Output: "lecoe"
+//     }
+// }
