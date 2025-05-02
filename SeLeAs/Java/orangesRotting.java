3289e4100b96b77d8637847d2ed95bf01edb4455
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-05-02 15:14:17 +0530

    LC147

---
+++ SeLeAs/Java/orangesRotting.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class orangesRotting {
+    
+
+public class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int fresh = 0;
+        Queue<int[]> rotten = new LinkedList<>();
+
+        // Count fresh oranges and enqueue all initially rotten ones
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    fresh++;
+                } else if (grid[r][c] == 2) {
+                    rotten.offer(new int[]{r, c});
+                }
+            }
+        }
+
+        if (fresh == 0) return 0; // No fresh oranges to start with
+
+        int minutes = 0;
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        // BFS
+        while (!rotten.isEmpty() && fresh > 0) {
+            int size = rotten.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = rotten.poll();
+                int r = curr[0];
+                int c = curr[1];
+
+                for (int[] dir : directions) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+                    if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        rotten.offer(new int[]{nr, nc});
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+}
+
+}
