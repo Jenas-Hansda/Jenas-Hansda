225c87b4cf3771adb8084e8329221bceebb168da
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-05-17 08:06:47 +0530

    LC167

---
+++ SeLeAs/Java/singleNumber.java
@@ -0,0 +1,14 @@
+
+
+public class singleNumber{
+    class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int x : nums) {
+            ans ^= x;
+        }
+        return ans;
+    }
+}
+
+}
