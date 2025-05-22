157dac1ea1e24e3b12f3c0688f0d9116dd1e2c48
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-05-22 06:26:15 +0530

    LC173

---
+++ SeLeAs/Java/dailyTemperatures.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class dailyTemperatures{
+    
+
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
+
+}
