86b6c65040d9c562e5ec1736c1bf024e52de6d4f
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-05-24 09:22:14 +0530

    LC176

---
+++ SeLeAs/Java/Top 100 Liked/binaryTreeInorderTraversal.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class binaryTreeInorderTraversal {
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+
+        return result;
+    }
+}
+
+}
