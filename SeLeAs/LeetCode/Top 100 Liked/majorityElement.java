bbea7221d251818d4837253d505ca2afc404b8f5
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-05-31 06:58:09 +0530

    LC190

---
+++ SeLeAs/LeetCode/Top 100 Liked/majorityElement.java
@@ -0,0 +1,18 @@
+public class majorityElement {
+    class Solution {
+    public int majorityElement(int[] nums) {
+        Integer element = null;
+        int counts = 0;
+
+        for (int num : nums) {
+            if (counts == 0) {
+                element = num;
+            }
+            counts += (num == element) ? 1 : -1;
+        }
+
+        return element;
+    }
+}
+
+}
