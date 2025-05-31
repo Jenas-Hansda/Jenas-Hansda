a3873765c7f749e87e306e59f86f02abda4e536a
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-05-31 06:33:56 +0530

    LC189

---
+++ SeLeAs/LeetCode/Top 100 Liked/validParentheses.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
+
+public class validParentheses {
+    
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || map.get(stack.pop()) != c) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
+
+}
