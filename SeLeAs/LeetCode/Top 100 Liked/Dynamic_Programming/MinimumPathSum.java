98cfd73cf1dde235fe9b55cea628e9d6646e3f5e
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-06-18 07:40:26 +0530

    LC208

---
+++ SeLeAs/LeetCode/Top 100 Liked/Dynamic_Programming/MinimumPathSum.java
@@ -0,0 +1,24 @@
+package Dynamic_Programming;
+
+public class MinimumPathSum {
+    class Solution {
+    public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (row == 0 && col != 0) {
+                    grid[row][col] += grid[row][col - 1];
+                } else if (col == 0 && row != 0) {
+                    grid[row][col] += grid[row - 1][col];
+                } else if (row != 0 && col != 0) {
+                    grid[row][col] += Math.min(grid[row - 1][col], grid[row][col - 1]);
+                }
+            }
+        }
+        return grid[rows - 1][cols - 1];
+    }
+}
+
+}
