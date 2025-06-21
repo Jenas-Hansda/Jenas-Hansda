8ac44da46ace5614ed68d8a34f3e9f1557095c08
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-06-21 06:46:15 +0530

    LC211

---
+++ SeLeAs/LeetCode/Top 100 Liked/Dynamic_Programming/WordBreak.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class WordBreak {
+    
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        List<Integer> trues = new ArrayList<>();
+        trues.add(0);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j : trues) {
+                if (wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    trues.add(i);
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
+
+}
