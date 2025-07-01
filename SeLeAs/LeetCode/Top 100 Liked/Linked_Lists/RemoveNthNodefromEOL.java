765f0c0c7c58b043ec2a1d50b20fc59c7a3b2e83
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-07-01 09:11:34 +0530

    LC228

---
+++ SeLeAs/LeetCode/Top 100 Liked/Linked_Lists/RemoveNthNodefromEOL.java
@@ -0,0 +1,47 @@
+
+public class RemoveNthNodefromEOL{
+
+    // Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}  // Default constructor
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // Create a dummy node that points to the head
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        // Move first n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        // Remove the nth node from the end
+        second.next = second.next.next;
+
+        return dummy.next;
+    }
+}
+}
