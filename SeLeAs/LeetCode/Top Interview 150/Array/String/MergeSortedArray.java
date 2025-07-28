43c9173fa212d3e0eba8733b08d6c26258d12d7b
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-07-28 06:01:02 +0530

    LC255

---
+++ SeLeAs/LeetCode/Top Interview 150/Array/String/MergeSortedArray.java
@@ -0,0 +1,22 @@
+package Array.String;
+
+
+class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;         // Pointer to end of valid nums1
+        int j = n - 1;         // Pointer to end of nums2
+        int k = m + n - 1;     // Pointer to end of merged nums1
+
+        // Merge from the back
+        while (i >= 0 && j >= 0) {
+            nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+        }
+
+        // If nums2 has leftover elements
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+
+        // No need to copy remaining nums1 elements; they're already in place
+    }
+}
