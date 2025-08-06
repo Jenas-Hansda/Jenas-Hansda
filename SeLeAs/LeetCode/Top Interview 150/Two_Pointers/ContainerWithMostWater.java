fb7b94d20f9a18fed35b1ce1c4dca05d4c27e708
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-08-06 06:42:04 +0530

    LC264

---
+++ SeLeAs/LeetCode/Top Interview 150/Two_Pointers/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package Two_Pointers;
+
+class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxWater = 0;
+        int lp = 0, rp = height.length - 1;
+
+        while (lp < rp) {
+            int w = rp - lp;
+            int ht = Math.min(height[lp], height[rp]);
+            int curWater = w * ht;
+            maxWater = Math.max(maxWater, curWater);
+
+            
+            if (height[lp] < height[rp]) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+        return maxWater;
+    }
+}
