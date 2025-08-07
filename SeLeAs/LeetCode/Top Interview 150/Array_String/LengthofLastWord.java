024834f17722d5d94f7fe559b95386b5b4c37270
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-08-07 05:56:26 +0530

    LC265

---
+++ SeLeAs/LeetCode/Top Interview 150/Array_String/LengthofLastWord.java
@@ -0,0 +1,24 @@
+package Array_String;
+
+public class LengthofLastWord {
+    public class LengthOfLastWord {
+
+  public static int lengthOfLastWord(String s) {
+    if (s == null) return 0;
+
+    int count = 0;
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+      if (s.charAt(i) != ' ') {
+        count++;
+      } else {
+        if (count > 0) return count;
+      }
+    }
+
+    return count;
+  }
+
+}
+
+}
