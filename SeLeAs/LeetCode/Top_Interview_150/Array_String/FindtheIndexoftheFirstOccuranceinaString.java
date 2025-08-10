336ec707cc8b3efab7e45d4308d1517ae1540a61
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-08-10 08:22:10 +0530

    LC268

---
+++ SeLeAs/LeetCode/Top_Interview_150/Array_String/FindtheIndexoftheFirstOccuranceinaString.java
@@ -0,0 +1,25 @@
+package Top_Interview_150.Array_String;
+
+public class FindtheIndexoftheFirstOccuranceinaString {
+    class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+
+        if (n == 0) return 0; // edge case: empty needle
+
+        for (int i = 0; i <= m - n; i++) {
+            int j = 0;
+            while (j < n && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == n) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
+}
