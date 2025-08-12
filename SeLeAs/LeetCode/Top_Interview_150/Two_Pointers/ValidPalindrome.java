56d264282c027d99bb167e8cf14cfdded1298afe
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-08-12 20:02:11 +0530

    LC270

---
+++ SeLeAs/LeetCode/Top_Interview_150/Two_Pointers/ValidPalindrome.java
@@ -0,0 +1,31 @@
+package Top_Interview_150.Two_Pointers;
+
+public class ValidPalindrome {
+    
+
+  public boolean validPalindrome(String s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+      // Skip non-alphanumeric characters
+      while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+        left++;
+      }
+      while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+        right--;
+      }
+
+      // Compare ignoring case
+      if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+        return false;
+      }
+
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+
+}
