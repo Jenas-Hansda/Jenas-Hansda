78969e3438df88ff1b152bf3d9b20406c712f61f
Jenas-Hansda <vishaljenashansda29@gmail.com>
2025-08-23 09:03:02 +0530

    LC281

---
+++ SeLeAs/LeetCode/Top_Interview_150/Linked_List/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,24 @@
+package Top_Interview_150.Linked_List;
+
+class RemoveNthNodeFromEndofList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+
+        return dummy.next;
+    }
+}
