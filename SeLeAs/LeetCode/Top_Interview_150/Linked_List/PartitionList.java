33dc98652322bbe3c99630cc20bd043d33aa6139
Jenas Hansda <vishaljenashansda29@gmail.com>
2025-09-01 07:01:40 +0530

    LC290

---
+++ SeLeAs/LeetCode/Top_Interview_150/Linked_List/PartitionList.java
@@ -0,0 +1,34 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode large = new ListNode(0);
+        
+        ListNode smallP = small;
+        ListNode largeP = large;
+        
+        while (head != null) {
+            if (head.val < x) {
+                smallP.next = head;
+                smallP = smallP.next;
+            } else {
+                largeP.next = head;
+                largeP = largeP.next;
+            }
+            head = head.next;
+        }
+        
+        smallP.next = large.next;
+        largeP.next = null;
+        
+        return small.next;
+    }
+}
