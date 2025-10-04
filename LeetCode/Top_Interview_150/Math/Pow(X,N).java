af6d2ae115602f657b3002b83b8c9fd17682b0e2
Jenas Hansda <vishaljenashansda29@gmail.com>
2025-10-04 20:25:58 +0530

    LC323

---
+++ LeetCode/Top_Interview_150/Math/Pow(X,N).java
@@ -0,0 +1,19 @@
+class Pow(X,N) {
+    
+    public double solve(double x, long n) {
+        if (n == 0)
+            return 1.0;
+
+        if (n < 0)
+            return 1.0 / solve(x, -n);
+
+        if (n % 2 == 0)
+            return solve(x * x, n / 2);
+
+        return x * solve(x * x, (n - 1) / 2);
+    }
+
+    public double myPow(double x, int n) {
+        return solve(x, (long) n); // Cast to long to safely handle Integer.MIN_VALUE
+    }
+}
