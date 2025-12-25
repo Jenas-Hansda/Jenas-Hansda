bb549cb39e56d755a229e4e5556aa9e84f0ebe1c
Jenas Hansda <vishaljenashansda29@gmail.com>
2025-12-25 17:25:00 +0530

    LC405

---
+++ LeetCode/Top_Interview_150/Multidimensional_DP/MaximalSquare.java
@@ -0,0 +1,38 @@
+public class MaximalSquare {
+
+    public static int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+        int maxSide = 0;
+
+        // Fill the dp table
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+
+                    // First row or first column
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 1 + Math.min(
+                                Math.min(dp[i - 1][j], dp[i][j - 1]),
+                                dp[i - 1][j - 1]
+                        );
+                    }
+
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+
+        // Return area of the largest square
+        return maxSide * maxSide;
+    }
+
+    
+}
