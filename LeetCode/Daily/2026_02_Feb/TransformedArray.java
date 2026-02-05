bcbb39e1cefab2d929340a19d9e953b1761b582c
Jenas Hansda <vishaljenashansda29@gmail.com>
2026-02-05 13:23:34 +0530

    LC445

---
+++ LeetCode/Daily/2026_02_Feb/TransformedArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class TransformedArray {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int shift = nums[i] % n; // reduce large shifts
+
+            int newIdx = (i + shift) % n;
+
+            if (newIdx < 0) {
+                newIdx += n;
+            }
+
+            result[i] = nums[newIdx];
+        }
+
+        return result;
+    }
+}
