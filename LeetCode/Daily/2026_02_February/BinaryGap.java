5662cb511e4af9e6af326e191f8e4129382b25e2
Jenas Hansda <vishaljenashansda29@gmail.com>
2026-02-22 20:19:43 +0530

    LC460

---
+++ LeetCode/Daily/2026_02_February/BinaryGap.java
@@ -0,0 +1,17 @@
+class BinaryGap {
+    public int binaryGap(int n) {
+        int prev = -1;
+        int result = 0;
+
+        for (int curr = 0; curr < 32; curr++) {
+            if (((n >> curr) & 1) == 1) {
+                if (prev != -1) {
+                    result = Math.max(result, curr - prev);
+                }
+                prev = curr;
+            }
+        }
+
+        return result;
+    }
+}
