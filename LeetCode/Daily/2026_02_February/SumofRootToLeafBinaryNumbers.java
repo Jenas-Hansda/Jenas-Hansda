dbbc82f8059661ebc50c83d6767c1c12de49c0ca
Jenas Hansda <vishaljenashansda29@gmail.com>
2026-02-24 20:08:29 +0530

    LC462

---
+++ LeetCode/Daily/2026_02_February/SumofRootToLeafBinaryNumbers.java
@@ -0,0 +1,20 @@
+class SumofRootToLeafBinaryNumbers {
+
+    public int solve(TreeNode root, int val) {
+        if (root == null) {
+            return 0;
+        }
+
+        val = (2 * val) + root.val;
+
+        if (root.left == null && root.right == null) {
+            return val;
+        }
+
+        return solve(root.left, val) + solve(root.right, val);
+    }
+
+    public int sumRootToLeaf(TreeNode root) {
+        return solve(root, 0);
+    }
+}
