e7d94da9fe5d13bbd142e8139fe2a14938da2177
Jenas Hansda <vishaljenashansda29@gmail.com>
2026-03-03 21:21:41 +0530

    LC470

---
+++ LeetCode/Daily/2026_03_March/FindKthBitinNthBinaryString.java
@@ -0,0 +1,22 @@
+class FindKthBitinNthBinaryString {
+    public char findKthBit(int n, int k) {
+        
+        if (n == 1) {
+            return '0';
+        }
+
+        int length = (1 << n) - 1;   // 2^n - 1
+        int mid = (length / 2) + 1;  // middle position
+
+        if (k < mid) {
+            return findKthBit(n - 1, k);
+        } 
+        else if (k == mid) {
+            return '1';
+        } 
+        else {
+            char ch = findKthBit(n - 1, length - k + 1); // reversed index
+            return (ch == '0') ? '1' : '0'; // flipped bit
+        }
+    }
+}
