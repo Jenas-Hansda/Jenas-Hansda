a9af181d7f527ba9bfb72c340ee483e6a7269acf
Jenas Hansda <vishaljenashansda29@gmail.com>
2026-03-08 21:09:18 +0530

    LC475

---
+++ LeetCode/Daily/2026_03_March/FindUniqueBinaryString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class FindUniqueBinaryString {
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            // Flip the diagonal character
+            char ch = nums[i].charAt(i);
+            result.append(ch == '0' ? '1' : '0');
+        }
+
+        return result.toString();
+    }
+}
