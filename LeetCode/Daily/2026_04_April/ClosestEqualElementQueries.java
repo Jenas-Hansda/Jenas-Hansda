de8319c53d26cdb2dadc6dec6bde1f57dc078213
Jenas-Hansda <vishaljenashansda29@gmail.com>
2026-04-17 22:49:47 +0530

    LC514

---
+++ LeetCode/Daily/2026_04_April/ClosestEqualElementQueries.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+
+        Map<Integer, List<Integer>> mp = new HashMap<>();
+
+        // Build map: value -> list of indices
+        for (int i = 0; i < n; i++) {
+            mp.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int qi : queries) { // O(Q)
+            int element = nums[qi];
+            List<Integer> vec = mp.get(element);
+
+            int sz = vec.size();
+
+            // Only one occurrence
+            if (sz == 1) {
+                result.add(-1);
+                continue;
+            }
+
+            // Binary search (lower_bound equivalent)
+            int pos = Collections.binarySearch(vec, qi);
+            if (pos < 0) {
+                pos = -pos - 1; // standard adjustment
+            }
+
+            int res = Integer.MAX_VALUE;
+
+            // Right neighbor
+            int right = vec.get((pos + 1) % sz);
+            int d = Math.abs(qi - right);
+            int circularDist = n - d;
+            res = Math.min(res, Math.min(d, circularDist));
+
+            // Left neighbor
+            int left = vec.get((pos - 1 + sz) % sz);
+            d = Math.abs(qi - left);
+            circularDist = n - d;
+            res = Math.min(res, Math.min(d, circularDist));
+
+            result.add(res);
+        }
+
+        return result;
+    }
+}
