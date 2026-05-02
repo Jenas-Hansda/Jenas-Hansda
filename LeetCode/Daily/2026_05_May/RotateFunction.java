556b04f6e2fe3a7f2216903fa5d3c6afd9d97c8e
Jenas-Hansda <vishaljenashansda29@gmail.com>
2026-05-02 20:35:51 +0530

    LC529

---
+++ LeetCode/Daily/2026_05_May/RotateFunction.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+
+        int sum = 0;
+        int F = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            F += i * nums[i];
+        }
+
+        int result = F;
+
+        for (int k = 1; k < n; k++) {
+            int newF = F + sum - n * nums[n - k];
+            result = Math.max(result, newF);
+            F = newF;
+        }
+
+        return result;
+    }
+}
