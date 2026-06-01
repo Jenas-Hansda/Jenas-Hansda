26e39080dd97caeefb38796e12e0bd98d6081637
Jenas-Hansda <vishaljenashansda29@gmail.com>
2026-06-01 21:33:11 +0530

    LC554

---
+++ LeetCode/Daily/2026_05_May/DestroyingAsteroids.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+
+        long totalMass = mass;
+
+        for (int asteroid : asteroids) {
+            if (totalMass < asteroid) {
+                return false;
+            }
+
+            totalMass += asteroid;
+        }
+
+        return true;
+    }
+}
